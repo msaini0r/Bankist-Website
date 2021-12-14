4a0618e058bc4ebeb5000eb3232ba75875165445
msaini0r <msaini0r@gmail.com>
2021-12-14 12:21:56 +0530

    changes

--- script.js
+++ script.js
@@ -93,6 +93,7 @@ tabsContainer.addEventListener("click", function (e) {
 const handleHover = function (e) {
   if (e.target.classList.contains("nav__link")) {
     const link = e.target;
+    // selecting parent then children
     const siblings = link.closest(".nav").querySelectorAll(".nav__link");
     const logo = link.closest(".nav").querySelector("img");
 
@@ -104,139 +105,63 @@ const handleHover = function (e) {
 };
 
 // Passing "argument" into handler
+
+// nav.addEventListener("mouseover", function (e) {
+//   handleHover(e, 0.5);
+// });
+
+// nav.addEventListener("mouseout", function (e) {
+//   handleHover(e, 1);
+// });
+
 nav.addEventListener("mouseover", handleHover.bind(0.5));
 nav.addEventListener("mouseout", handleHover.bind(1));
 
+//////////////////////////////////////////////////////////////////////////////
+
+// Sticky Navigation: Intersection API
+
+const header = document.querySelector(".header");
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+headerObserver.observe(header);
+//////////////////////////////////////////////////////
+
+// Reveal Section
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
 ///////////////////////////////////////////////////////
-// /* Selecting Documents*/
-
-// console.log(document.documentElement);
-// console.log(document.head);
-// console.log(document.body);
-
-// const header = document.querySelector(".header");
-// const allSections = document.querySelectorAll(".section");
-// console.log(allSections);
-
-// // Other methods to select the elements
-
-// // The difference between the two is elementbytagname is a html eleent it means whenver something changes in the code it automatically changes.
-// // However in case of elementbyid is a nodelist , it means the the changes doesnot show itself.
-// const Section = document.getElementById("section--1");
-// console.log(Section);
-// const allButtons = document.getElementsByTagName("button");
-// console.log(allButtons);
-
-// console.log(document.getElementsByClassName("btn"));
-
-// // Creating and inserting elements
-// const message = document.createElement("div");
-// message.classList.add("cookie-message");
-// // message.textContent = 'We use cookied for improved functionality and analytics.';
-// message.innerHTML =
-//   'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
-// // header.prepend(message);
-// header.append(message);
-// // header.append(message.cloneNode(true));
-// // header.before(message);
-// // header.after(message);
-// // Delete elements
-// document
-//   .querySelector(".btn--close-cookie")
-//   .addEventListener("click", function () {
-//     // message.remove();
-//     message.parentElement.removeChild(message);
-//   });
-
-// //Styles
-
-// message.style.backgroundColor = "#37383d";
-// message.style.width = "120%";
-
-// console.log(message.style.backgroundColor);
-
-// document.documentElement.style.setProperty("--color-primary", "orangered");
-
-// // Attributes
-// const logo = document.querySelector(".nav__logo");
-// console.log(logo.alt);
-// console.log(logo.className);
-// logo.alt = "Beautiful minimalist logo";
-// // Non-standard
-// console.log(logo.designer);
-// console.log(logo.getAttribute("designer"));
-// logo.setAttribute("company", "Bankist");
-// console.log(logo.src);
-// console.log(logo.getAttribute("src"));
-// const link = document.querySelector(".nav__link--btn");
-// console.log(link.href);
-// console.log(link.getAttribute("href"));
-// // Data attributes
-// console.log(logo.dataset.versionNumber);
-// // Classes
-// logo.classList.add("c", "j");
-// logo.classList.remove("c", "j");
-// logo.classList.toggle("c");
-// logo.classList.contains("c"); // not includes
-// // Don't use
-// logo.clasName = "jonas";
-
-/////////////////////////////////////////////////////
-
-//Types of Events and Event Handlers
-
-// const h1 = document.querySelector("h1")
-// const alertH1 = function (e) {
-//   alert("addEventListener: Greta: You are Reading the heading")
-// }
-
-// h1.addEventListener("mouseenter", alertH1)
-// setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000);
-
-/////////////////////////////////////////////////////////////
-
-// Event Propagation in Practice
-// const randomInt = (min, max) =>
-//   Math.floor(Math.random() * (max - min + 1) + min);
-// const randomColor = () =>
-//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-// document.querySelector(".nav__link").addEventListener("click", function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log("LINK", e.target, e.currentTarget);
-//   console.log(e.currentTarget === this);
-//   // Stop propagation
-//   // e.stopPropagation();
-// });
-// document.querySelector(".nav__links").addEventListener("click", function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log("CONTAINER", e.target, e.currentTarget);
-// });
-// document.querySelector(".nav").addEventListener("click", function (e) {
-//   this.style.backgroundColor = randomColor();
-//   console.log("NAV", e.target, e.currentTarget);
-// });
-
-///////////////////////////////////////////////
-
-// DOM Traversing
-// const h1 = document.querySelector('h1');
-// // Going downwards: child
-// console.log(h1.querySelectorAll('.highlight'));
-// console.log(h1.childNodes);
-// console.log(h1.children);
-// h1.firstElementChild.style.color = 'white';
-// h1.lastElementChild.style.color = 'orangered';
-// // Going upwards: parents
-// console.log(h1.parentNode);
-// console.log(h1.parentElement);
-// h1.closest('.header').style.background = 'var(--gradient-secondary)';
-// h1.closest('h1').style.background = 'var(--gradient-primary)';
-// // Going sideways: siblings
-// console.log(h1.previousElementSibling);
-// console.log(h1.nextElementSibling);
-// console.log(h1.previousSibling);
-// console.log(h1.nextSibling);
-// console.log(h1.parentElement.children);
-// [...h1.parentElement.children].forEach(function (el) {
-//   if (el !== h1) el.style.transform = 'scale(0.5)';
-// });
